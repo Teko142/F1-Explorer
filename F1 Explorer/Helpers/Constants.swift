767a23c4de4dc050330e59c57f6f6929b9f5ce0d
Arthur <olehno.arturs@gmail.com>
2023-12-05 13:36:17 +0200

    Add Constants

---
+++ F1 Explorer/Helpers/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  F1 Explorer
+//
+//  Created by Артур Олехно on 24/11/2023.
+//
+
+import Foundation
+
+struct Constants {
+    static let API_KEY = "371648ddf66da22b1395ba5151d75cff"
+    static let baseURL = "https://v1.formula-1.api-sports.io"
+    static let YouTubeAPI_KEY = "AIzaSyB6yW_gpKPrQQDhO5GLTdSxBWTgwop4TIo"
+    static let YouTubeBaseURL = "https://youtube.googleapis.com/youtube/v3/search?"
+    static let TranslateURL = "https://translation.googleapis.com/language/translate/v2?"
+    static let TranslateAPI_KEY = "AIzaSyC3LJ08d-gJKWPCzbKXd7wA4mhuI2W_jQY"
+}
